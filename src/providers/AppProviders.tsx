7fc796144f0032f52d03320d87cad1be450e3c3c
guilhermetamega <guitamega06@gmail.com>
2026-03-18 14:16:13 -0300

    Fix: Theme dark and light system default

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -1,7 +1,6 @@
 import type { ReactNode } from "react";
 import { AuthProvider } from "@/providers/AuthProvider";
 import { ProfileProvider } from "@/contexts/ProfileContext";
-import { ThemeProvider } from "./ThemeProvider";
 
 type AppProvidersProps = {
   children: ReactNode;
