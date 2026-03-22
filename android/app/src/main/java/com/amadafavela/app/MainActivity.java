8f6b97e039dd5506666c97a49ae8114ef3180f43
guilhermetamega <guitamega06@gmail.com>
2026-03-21 22:32:59 -0300

    Build: Creating first try of capacitor build for android

---
+++ android/app/src/main/java/com/amadafavela/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.amadafavela.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.amadafavela.app',
+  appName: 'AMA da Favela',
+  webDir: 'dist'
+};
+
+export default config;
