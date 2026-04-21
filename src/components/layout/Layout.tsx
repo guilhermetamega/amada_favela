bbc95d95925ee4628d82a8aec1c004405d800f21
guilhermetamega <guitamega06@gmail.com>
2026-04-20 23:55:21 -0300

    Fix: Layout background dark adjusted

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -11,7 +11,7 @@ type Props = {
 
 export default function Layout({ children, hasLogo = false }: Props) {
   return (
-    <div className="min-h-screen bg-zinc-50 text-zinc-900 transition-colors dark:bg-gradient-to-b dark:from-zinc-900 dark:via-emerald-950/10 dark:to-zinc-950 dark:text-zinc-100">
+    <div className="min-h-screen bg-zinc-50 text-zinc-900 transition-colors dark:bg-zinc-950 dark:text-zinc-100">
       <div className="hidden md:block">
         <SideBar />
       </div>
@@ -22,7 +22,7 @@ export default function Layout({ children, hasLogo = false }: Props) {
         </div>
 
         {hasLogo && (
-          <div className="block pb-38 bg-zinc-50 text-zinc-900 transition-colors dark:bg-gradient-to-b dark:from-zinc-900 dark:via-emerald-950/10 dark:to-zinc-950 dark:text-zinc-100">
+          <div className="block pb-38 bg-zinc-50 text-zinc-900 transition-colors dark:bg-zinc-950 dark:text-zinc-100">
             <p className="text-xs text-zinc-500 dark:text-zinc-400">
               Desenvolvido Pela Equipe das:
             </p>
